d629eacd9e8f5a61246a2cb86cdc15354744297d
xSarscov <vargasernest7@gmail.com>
2024-12-24 18:34:55 -0600

    feat: Add calendar component with date-fns and react-big-calendar events and dependencies. 🚧

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,48 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css"
 import { Navbar } from "../";
 
+
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+	"en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+});
+
+const events = [
+	{
+		title: "Cumpleaños del jefe",
+		notes: "Hay que comprar pastel",
+		start: new Date(),
+		end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      id: "123",
+      name: "Ernesto"
+    }
+	},
+];
+
 export const CalendarPage = () => {
-  return (
-    <>
-    
-      <Navbar />
-    
-    </>
-  )
-}
+	return (
+		<>
+			<Navbar />
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				style={{ height: 500 }}
+			/>
+		</>
+	);
+};
